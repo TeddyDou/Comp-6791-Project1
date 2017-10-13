878f7bf121adc93dc92b6f4d78de413dd8e58c93
Teddy <ee08b0081@163.com>
2017-10-12 22:34:25 -0400

    A refined new implementation of spimi algorithm

--- oldspimi.py
+++ oldspimi.py
@@ -2,14 +2,13 @@ import sys
 import os
 import cPickle
 
-memorySizeLimit = 0.5 * 1024 * 1024
+memorySizeLimit = 0.5*1024*1024
 blockSizeLimit = 512
 global DocID
 DocID = 0
 spimiFileNumber = 0
 global endInput
 endInput = False
-
 
 def spimi_invert():
     """The SPIMI algorithm method to generate inverted-index"""
@@ -22,10 +21,10 @@ def spimi_invert():
     dictionary = {}
     input_stream = []
 
-    while ((memorySizeLimit - sys.getsizeof(dictionary))) >= blockSizeLimit:
+    while((memorySizeLimit - sys.getsizeof(dictionary))) >= blockSizeLimit:
         # Simulation of checking if the available memory is enough for the next block
 
-        # print 'dictionary memory size is ' + str(sys.getsizeof(dictionary)) + ' byte'
+        #print 'dictionary memory size is ' + str(sys.getsizeof(dictionary)) + ' byte'
         while len(input_stream) == 0:
             print 'file ID: ' + str(DocID)
             print 'dictionary memory size is ' + str(sys.getsizeof(dictionary)) + ' byte'
@@ -51,9 +50,9 @@ def get_input():
     global DocID
     global endInput
     DocID += 1
-    filename = 'file_terms/' + str(DocID) + '.txt'
+    filename = 'file_terms/'+ str(DocID) + '.txt'
     if os.path.isfile(filename):
-        with open(filename, 'r') as fp:
+        with open(filename,'r') as fp:
             input_stream = cPickle.load(fp)
         fp.close
     else:
@@ -102,74 +101,37 @@ def merge_spimi_file():
         if len(files) > 1:
             for doc in os.listdir(rootdir):
                 if os.path.splitext(doc)[1] == '.txt':
-                    with open(rootdir + '/' + doc, 'r') as fp:
+                    with open(rootdir+'/'+doc,'r') as fp:
                         terms.append(cPickle.load(fp))
                         dictionaries.append(cPickle.load(fp))
                     fp.close
 
             for index, term_list in enumerate(terms[1:]):
-                print term_list, index + 1
+                print term_list, index+1
                 for term in term_list:
                     if term in terms[0]:
                         postings1 = dictionaries[0][term]
-                        postings2 = dictionaries[index + 1][term]
+                        postings2 = dictionaries[index+1][term]
                         new_postings = postings1 + postings2
                         dictionaries[0][term] = new_postings
                     else:
                         terms[0].append(term)
-                        dictionaries[0][term] = dictionaries[index + 1][term]
+                        dictionaries[0][term] = dictionaries[index+1][term]
 
             sorted_terms = sort_terms(dictionaries[0])
-            fp = open('inverted index/FinalSpimi.txt', 'w')
+            fp = open('inverted index/FinalSpimi.txt','w')
             write_to_disk(sorted_terms, dictionaries[0], fp)
             fp.close()
         else:
             print 'Only 1 spimi file, no need to merge, please refer to:' + root + files
 
 
-def begin_spimi():
-    spimiFileNumber = 0
-    DocID = 0
-    endInput = False
-    input_stream = []
-
-    while not endInput:
-        dictionary = {}
-        while (memorySizeLimit - sys.getsizeof(dictionary)) >= blockSizeLimit:
-            if input_stream ==[]:
-                DocID += 1
-                filename = 'file_terms/' + str(DocID) + '.txt'
-                if os.path.isfile(filename):
-                    with open(filename, 'r') as termfile:
-                        input_stream = read_from_dist(termfile)
-                else:
-                    endInput = True
-                    break
-                termfile.close()
-            else:
-                token = input_stream.pop(0)
-                term, doc_id = token[0], token[1]
-                if term not in dictionary:
-                    postings_list = add_to_dictionary(dictionary, term)
-                else:
-                    postings_list = get_postings_list(dictionary, term)
-                add_to_postings_list(postings_list, doc_id)
-        sorted_terms = sort_terms(dictionary)
-        # sorted_terms = sorted(dictionary, key=lambda postings: postings[0])
-        spimiFileNumber += 1
-        spimi_file = open("inverted index/spimi" + str(spimiFileNumber) + ".txt", 'w')
-        write_to_disk(sorted_terms, dictionary, spimi_file)
-        print 'Created file: ' + str(spimi_file)
-        print 'Files processed: ' + str(DocID-1)
-        spimi_file.close()
-
-
-
 def main1():
     if not os.path.isdir("inverted index"):
         os.mkdir("inverted index")
-    begin_spimi()
-
+    while not endInput:
+        print 'begin to generate time: ' + str(spimiFileNumber)
+        spimi_invert()
 
 def main6():
     if not os.path.isdir("inverted index"):
@@ -178,7 +140,6 @@ def main6():
     spimi_invert()
     print DocID
 
-
 def main2():
     ft = open('inverted index/FinalSpimi.txt', 'r')
     mylist = read_from_dist(ft)
@@ -186,19 +147,16 @@ def main2():
     for item in mylist:
         print item + ': ' + str(mydic[item])
 
-
 def main3():
-    ft = open('inverted index/FinalSpimi.txt', 'r')
+    ft = open('inverted index/spimi1_MM1.txt', 'r')
     mylist = read_from_dist(ft)
     mydic = read_from_dist(ft)
     print mylist
     print len(mylist)
     ft.close()
 
-
 def main4():
     merge_spimi_file()
-
 
 def main5():
     # file1 = open('inverted index/spimi1_MM1.txt', 'r')
@@ -223,6 +181,5 @@ def main5():
             if 'westard' in l:
                 print doc
 
-
 if __name__ == "__main__":
-    main3()
+    main6()
